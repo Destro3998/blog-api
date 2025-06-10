0b50c7b584311c418c00a8ef5372ca716dc5a538
Destro3998 <64928019+Destro3998@users.noreply.github.com>
2025-06-10 17:55:16 -0600

    updating text to test CI/CD workflow on GitHub action

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Blog API!"
+    return "Hello from Blog API! - this is v1 update here to test CI/CD"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
